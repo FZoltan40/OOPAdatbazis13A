a8ab977ef9821e0d4916d954f1daf446262902ae
FZoltan40 <farkasz@kkszki.hu>
2025-09-19 11:28:42 +0200

    GetById() interface able to call.

--- OOPAdatbazis/OOPAdatbazis/Program.cs
+++ OOPAdatbazis/OOPAdatbazis/Program.cs
@@ -10,13 +10,16 @@ namespace OOPAdatbazis
             Console.Write("Kérem az datbázis nevét: ");
             string dbName = Console.ReadLine();
 
-            ISqlStatements dataBase = new Cars();
+            ISqlStatements dataBase = new Library();
 
-            foreach (var item in dataBase.GetAllData(dbName))
+            /*foreach (var item in dataBase.GetAllData(dbName))
             {
                 var books = item.GetType().GetProperties();
                 Console.WriteLine($"{books[0].Name}={books[0].GetValue(item)}, {books[1].Name}={books[1].GetValue(item)}");
-            }
+            }*/
+
+            Console.Write("Kérem a rekord id-jét: ");
+            Console.WriteLine(dataBase.GetById(int.Parse(Console.ReadLine())));
 
         }
     }
