d28fe70bdf2c334e3d658cc66e86171f90c05ee4
FZoltan40 <farkasz@kkszki.hu>
2025-09-19 11:27:33 +0200

    Library GetById() ready to use.

--- OOPAdatbazis/OOPAdatbazis/Services/Library.cs
+++ OOPAdatbazis/OOPAdatbazis/Services/Library.cs
@@ -5,6 +5,15 @@ namespace OOPAdatbazis.Services
 {
     internal class Library : ISqlStatements
     {
+        public object AddNewItem(object newRecord)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public object DeleteItem(int id)
+        {
+            throw new System.NotImplementedException();
+        }
 
         public List<object> GetAllData(string dbName)
         {
@@ -26,10 +35,10 @@ namespace OOPAdatbazis.Services
             {
                 var book = new
                 {
-                    id = dr.GetInt32("id"),
-                    title = dr.GetString("title"),
-                    author = dr.GetString("author"),
-                    releaseDate = dr.GetDateTime("releaseDate")
+                    id = dr.GetBodyDefinition(dr.GetName(0)),
+                    title = dr.GetBodyDefinition(dr.GetName(1)),
+                    author = dr.GetBodyDefinition(dr.GetName(2)),
+                    releaseDate = dr.GetBodyDefinition(dr.GetName(3))
                 };
 
                 books.Add(book);
@@ -38,6 +47,40 @@ namespace OOPAdatbazis.Services
             conn.Connection.Close();
 
             return books;
+        }
+
+        public object GetById(int id)
+        {
+            Connect conn = new Connect("library");
+
+            conn.Connection.Open();
+
+            string sql = "SELECT * FROM `books` WHERE id = @id";
+
+            MySqlCommand cmd = new MySqlCommand(sql, conn.Connection);
+
+            cmd.Parameters.AddWithValue("@id", id);
+
+            MySqlDataReader dr = cmd.ExecuteReader();
+
+            dr.Read();
+
+            var record = new
+            {
+                id = dr.GetBodyDefinition(dr.GetName(0)),
+                title = dr.GetBodyDefinition(dr.GetName(1)),
+                author = dr.GetBodyDefinition(dr.GetName(2)),
+                releaseDate = dr.GetBodyDefinition(dr.GetName(3))
+            };
+
+            conn.Connection.Close();
+
+            return record;
+        }
+
+        public object UpdateItem(object modifiedItem)
+        {
+            throw new System.NotImplementedException();
         }
     }
 }
